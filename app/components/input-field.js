1d20cdd7969b115acfafc5310a2c7b730f0c7ace
Stepan Sych <stepansych@gmail.com>
2022-08-08 10:56:44 +0300

    Add response from api

--- app/components/input-field.js
+++ app/components/input-field.js
@@ -1,8 +1,8 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
-export default class Convert extends Component {
+export default class ConverterComponent extends Component {
   @tracked result = "";
   @tracked value = "";
 
@@ -10,12 +10,19 @@ export default class Convert extends Component {
   convert(eventClick) {
     eventClick.preventDefault();
     console.log(eventClick);
-    this.result = this.value;
+    this.result = this.value * 2;
   }
 
   @action
   inputValue(eventInput) {
     console.log(eventInput.data);
-    this.value += eventInput.data.toString();
+    this.value += eventInput.data.toString().replace(/[^\d.]/g, "");
   }
+
+  @action
+  selectCurrency(eventSelect) {
+    console.log(eventSelect.target.value);
+    this.selectValue = eventSelect.target.value;
+  }
+
 }
--- app/routes/converter.js
+++ app/routes/converter.js
@@ -1,3 +1,14 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
-export default class ConverterRoute extends Route {}
+export default class ConverterRoute extends Route {
+  model() {
+    return fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        return data
+      });
+  }
+}
